d42d4b2a42f64a32de3fb6db013988e2b0197c94
Bryan Green <twistedogre@gmail.com>
2019-04-22 17:31:42 -0700

    Added some implementation and tests

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 
-const pointInPolygon = ((polygon, point) => {
-  throw new Error('not implemented');
-});
+function isLeft(linePoint1, linePoint2, point) {
+  // return ((b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x)) > 0;
+  const isLeft = ((linePoint2.y - linePoint1.y) * (point.x - linePoint1.x) -
+                  (linePoint2.x - linePoint1.x) * (point.y - linePoint1.y)) > 0;
+  return isLeft;
+}
+
+function rayCrossesLine(linePoint1, linePoint2, point) {
+  const y1 = linePoint1.y;
+  const y2 = linePoint2.y;
+  console.log('between? ', point.x, y1, y2);
+  const between =  point.x >= Math.min(y1, y2) && point.y < Math.max(y1, y2);
+  console.log('left? ', linePoint1, linePoint2, point);
+  const left = isLeft(linePoint1, linePoint2, point);
+  console.log('between', between, 'left', left);
+  return between && left;
+}
+
+function countCrossings(polygonPointsArray, point) {
+  let crossings = 0;
+  polygonPoints = polygonPointsArray.map(point => ({x: point[0], y: point[1]}));
+  const len = polygonPoints.length;
+  for (let i = 0; i < len - 1; i++) {
+    const nextPoint = (i !== len) ? polygonPoints[i + 1] : polygonPoints[0];
+    crossings += rayCrossesLine(polygonPoints[i], nextPoint, point);
+  }
+  return crossings;
+}
+
+function pointInPolygon(polygonPoints, point) {
+  const crossings = countCrossings(polygonPoints, point);
+  console.log(crossings, crossings % 2 !== 0);
+  return (crossings % 2 !== 0);
+}
 
 module.exports = {
+  isLeft,
   pointInPolygon,
 };
--- test/test.js
+++ test/test.js
@@ -1,55 +1,82 @@
 const assert = require('assert');
+const expect = require('chai').expect;
 
 const polygon = require('../polygon-points.json');
 const server = require('../server');
 
 const knownInside = [
   {
-    lat: -118.3828353881836,
-    long: 34.079109226606356
+    y: -118.3828353881836,
+    x: 34.079109226606356
   },
   {
-    lat: -118.33030700683594,
-    long: 34.09602708232169
+    y: -118.33030700683594,
+    x: 34.09602708232169
   },
   {
-    lat: -118.4028353881836,
-    long: 34.0650209226606356
+    y: -118.4028353881836,
+    x: 34.0650209226606356
   }
 ];
 
 const knownOutside = [
   {
-    lat: -118.33000,
-    long: 34.11969051767928
+    y: -118.33000,
+    x: 34.11969051767928
   },
   {
-    lat: -118.4028353881836,
-    long: 34.0650209226606356
+    y: -118.4028353881836,
+    x: 34.0650209226606356
   }
 ];
 
+describe('Server', (() => {
+  it.skip('should be able to load the server and its functions', (() => {
+    assert(!!server);
+    assert(typeof server.pointInPolygon === "function");
+  }));
+}));
 
-describe('Server', function () {
-    it('should be able to load the server and its functions', function () {
-      assert(!!server);
-      assert(typeof server.pointInPolygon === "function");
-  });
-});
+describe('Polygons', (() => {
+  describe('Determine whether a point is in a polgon', (() => {
 
-describe('Polygons', function () {
-  describe('Determine whether a point is in a polgon', function () {
+    it.skip('should know if a point is left or right of a line', (() => {
+      const linePoint1 = { x: 0, y: 10 };
+      const linePoint2 = { x: 10, y: 0 };
+      let point = { x: 4.999, y: 5 };
+      server.isLeft(linePoint1, linePoint2, point);
+      expect(server.isLeft(linePoint1, linePoint2, point)).to.be.true;
+      point = { x: 15, y: 5 };
+      expect(server.isLeft(linePoint1, linePoint2, point)).to.be.false;
+      point = { x: 11, y: 5 };
+      expect(server.isLeft(linePoint1, linePoint2, point)).to.be.false;
+      point = { x: 9, y: 0 };
+      expect(server.isLeft(linePoint1, linePoint2, point)).to.be.true;
+    }));
 
-    it('should return true when the point is inside the polygon', function () {
-      knownInside.forEach((point) => {
-        assert.true(server.pointInPolygon(polygon, point));
+    it('should know if a point is left or right of a line', (() => {
+      const linePoint1 = { x: -10, y: 10 };
+      const linePoint2 = { x: 0, y: 0 };
+      let point = { x: -4, y: 5 };
+      server.isLeft(linePoint1, linePoint2, point);
+      expect(server.isLeft(linePoint1, linePoint2, point)).to.be.true;
+      point = { x: -6, y: 5 };
+      server.isLeft(linePoint1, linePoint2, point);
+      expect(server.isLeft(linePoint1, linePoint2, point)).to.be.true;
+    }));
+
+    it('should return true when the point is inside the polygon', (() => {
+      knownInside.forEach(point => {
+        console.log('THE REAL POINT-=>', point.y, point.x)
+        expect(server.pointInPolygon(polygon, point)).to.be.true;
       });
-    });
+    }));
 
-    it('should return false when the point is outside the polygon', function () {
+    it('should return false when the point is outside the polygon', (() => {
       knownOutside.forEach((point) => {
-        assert.false(server.pointInPolygon(polygon, point));
+        console.log('-=>', point.y, point.x)
+        expect(server.pointInPolygon(polygon, point)).to.be.false;
       });
-    });
-  });
-});
+    }));
+  }));
+}));
